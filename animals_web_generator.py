52884ba1056fbb7d5034b700650c0f6caaa1c224
Paul Lourdu Xavier <34255718+plxavier@users.noreply.github.com>
2025-09-24 12:20:25 +0200

    access data from json file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,3 +1,32 @@
+import json
+
+file_path = 'animals_data.json'
+
+
+
+
+def load_data(file_path):
+  """ Loads a JSON file """
+  with open(file_path, "r") as fobj:
+    return json.load(fobj)
+
+animals_data = load_data('animals_data.json')
+# print(animals_data)
+
+def print_data(anim_dic):
+    for animal in animals_data:
+        print(f"Name: {animal['name']}\n"
+              f"Diet: {animal['characteristics'].get('diet', 'N/A')}\n"
+              f"Location: {animal['locations']}\n"
+              f"Type: {animal['characteristics'].get('type', 'N/A')}"
+              f"\n"
+              )
+
+print_data(animals_data)
+
+
+
+
 def main():
     pass
 
